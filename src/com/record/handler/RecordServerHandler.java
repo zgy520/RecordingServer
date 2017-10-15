7031ab249c1a411d9750e7fcff2e05987034c318
unknown <a442391947@gmail.com>
2017-10-15 15:53:00 +0800

    First commit

---
+++ src/com/record/handler/RecordServerHandler.java
@@ -0,0 +1,20 @@
+package com.record.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+public class RecordServerHandler extends ChannelInboundHandlerAdapter {
+	@Override
+	public void channelRead(ChannelHandlerContext ctx,Object msg) {
+		ByteBuf in = (ByteBuf)msg;
+		System.out.println(in.toString(CharsetUtil.UTF_8));
+	}
+	
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
---
+++ src/com/record/server/RecordServer.java
@@ -0,0 +1,50 @@
+package com.record.server;
+
+import java.net.Inet4Address;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+
+import com.record.handler.RecordServerHandler;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.local.LocalAddress;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class RecordServer {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println(Inet4Address.getLocalHost().getHostAddress());
+		new RecordServer().start();
+	}
+	
+	public void start() throws Exception{
+		EventLoopGroup group = new NioEventLoopGroup();
+		try {
+			ServerBootstrap bootstrap = new ServerBootstrap();
+			bootstrap.group(group)
+					 .channel(NioServerSocketChannel.class)
+					 .localAddress(new InetSocketAddress(port))
+					 .childHandler(new ChannelInitializer<SocketChannel>() {
+
+						@Override
+						protected void initChannel(SocketChannel ch) throws Exception {
+							// TODO Auto-generated method stub
+							ch.pipeline().addLast(new RecordServerHandler());
+						}
+					});
+			ChannelFuture future = bootstrap.bind().sync();
+			future.channel().closeFuture().sync();
+		} finally {
+			// TODO: handle finally clause
+			group.shutdownGracefully();
+		}
+	}
+
+	private final static int port = 8089;  //ÉèÖÃ¼àÌýµÄ¶Ë¿ÚºÅ
+}
